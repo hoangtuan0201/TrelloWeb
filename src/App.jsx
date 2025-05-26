85fe3b7ed9c9fd717107a31050469940c24a3cd6
PC <hoangtuan0796991061@gmail.com>
2025-05-26 18:28:05 +0700

    dark_light_p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-
 import Button from '@mui/material/Button'
 import '@mui/material/Icon'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import Typography from '@mui/material/Typography'
+
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 function App() {
   return (
     <>
-
+      <ModeToggle />
       <h1>Material UI</h1>
       <div>hoang tuan dev</div>
       <Button variant="text" color="success">Text</Button>
